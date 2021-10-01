9e442bde89de85200e62c0ac50155d62c4118495
Raju Minhajul Abedin <rajuminhajulabedin@gmail.com>
2021-10-02 00:28:04 +0100

    Added option to reload a specific command file

---
+++ src/WebHook/CommandsWebHook.js
@@ -0,0 +1,68 @@
+module.exports = function (app, API_SECRET, shard) {
+    app.get('/commands', async (req, res) => {
+        const signature = req.headers['x-api-key'];
+
+        if (signature !== API_SECRET) {
+            return res.sendStatus(401);
+        }
+
+        const reqs = await shard.fetchClientValues('commands');
+
+        res.type('application/json').status(200).send(JSON.stringify({ "commands": reqs }));
+    });
+
+    app.post('/reload-command', async (req, res) => {
+        const signature = req.headers['x-api-key'];
+        const body = req.body;
+        let command = null;
+
+        if (signature !== API_SECRET) {
+            return res.sendStatus(401);
+        }
+
+        if (typeof body.fileName === 'undefined' || body.fileName === '') {
+            return res.type('application/json').status(500).send(JSON.stringify({ "status": 500, "error": "file name not specified" }));
+        }
+        Object.keys(require.cache).forEach(function (key) { delete require.cache[key] })
+        try {
+            command = require(`${process.cwd()}/Commands/${body.fileName}`);
+
+        } catch (e) {
+            return res.type('application/json').status(500).send(JSON.stringify({ "status": 500, "error": e.toString() }));
+        }
+
+        function funcName(client, { command }) {
+            client.commands.set(command.data.name, command)
+        }
+
+        await shard.broadcastEval(funcName, { context: { command: command } });
+
+        return res.type('application/json').status(200).send(JSON.stringify({ "status": 200, "update": "updated" }));
+
+    });
+
+    app.post('/reload', async (req, res) => {
+        const signature = req.headers['x-api-key'];
+
+        if (signature !== API_SECRET) {
+            return res.sendStatus(401);
+        }
+        await shard.broadcastEval(client => {
+            const Discord = require('discord.js');
+            const fs = require("fs");
+            const newCommands = new Discord.Collection();
+            // LOAD COMMANDS
+            fs.readdirSync(`${process.cwd()}/Commands`).filter(file => file.endsWith(".js")).forEach(file => {
+                Object.keys(require.cache).forEach(function (key) { delete require.cache[key] })
+                /**
+                 *@type {Command}
+                 */
+                const command = require(`${process.cwd()}/Commands/${file}`);
+                newCommands.set(command.data.name, command)
+            });
+            client.commands = newCommands;
+        })
+
+        res.type('application/json').status(200).send(JSON.stringify({ "status": 200, "update": "updated" }));
+    });
+}
--- src/shard.js
+++ src/shard.js
@@ -2,11 +2,20 @@ const { ShardingManager } = require('discord.js');
 require('dotenv').config()
 
 
+const express = require('express');
+const app = express();
+
+const API_SECRET = 'secret';
+
 
 const shard = new ShardingManager('./bot.js', {
     token: process.env.TOKEN,
     respawn: true,
 });
+
+
+app.use(express.json({ verify: (req, res, buffer) => { req.rawBody = buffer; } }));
+app.listen(9000, () => console.log('Node.js server started on port 9000.'));
 
 shard.spawn(); // Spawns recommended shards!
 
@@ -15,59 +24,14 @@ shard.on('shardCreate', newShard => {
     newShard.on('ready', () => {
         console.log('Shard ready')
     })
+    Object.keys(require.cache).forEach(function (key) { delete require.cache[key] })
+    require('./WebHook/CommandsWebHook.js')(app, API_SECRET, shard);
 })
 
 shard.on('launch', shard => console.log(`[SHARD] Shard ${shard.id}/${shard.totalShards}`));
 
-const express = require('express');
-const app = express();
-
-const API_SECRET = 'secret';
-
-app.use(express.json({ verify: (req, res, buffer) => { req.rawBody = buffer; } }));
-
-app.get('/commands', async (req, res) => {
-    const signature = req.headers['x-api-key'];
-
-    if (signature !== API_SECRET) {
-        return res.sendStatus(401);
-    }
-
-    const reqs = await shard.fetchClientValues('commands');
-
-    res.type('application/json').status(200).send(JSON.stringify({ "commands": reqs }));
-});
-
-app.post('/reload', async (req, res) => {
-    const signature = req.headers['x-api-key'];
-
-    if (signature !== API_SECRET) {
-        return res.sendStatus(401);
-    }
 
 
 
 
-    await shard.broadcastEval(c => {
-        const Discord = require('discord.js');
-        const fs = require("fs");
-        const newCommands = new Discord.Collection();
-        // LOAD COMMANDS
-        fs.readdirSync(`${process.cwd()}/Commands`).filter(file => file.endsWith(".js")).forEach(file => {
-            /**
-             *@type {Command}
-             */
 
-            Object.keys(require.cache).forEach(function (key) { delete require.cache[key] })
-            const command = require(`${process.cwd()}/Commands/${file}`);
-            newCommands.set(command.data.name, command)
-        });
-        c.commands = newCommands;
-    })
-
-    const reqs = await shard.fetchClientValues('commands');
-
-    res.type('application/json').status(200).send(JSON.stringify({ "commands": reqs }));
-});
-
-app.listen(9000, () => console.log('Node.js server started on port 9000.'));
