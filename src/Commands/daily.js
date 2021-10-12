a0e8f98e9244c4303985c9ac1d500d16a72a302e
Raju Minhajul Abedin <rajuminhajulabedin@gmail.com>
2021-10-12 12:40:08 +0100

    Added rewards on daily

--- src/Commands/daily.js
+++ src/Commands/daily.js
@@ -25,13 +25,19 @@ module.exports = {
             const diffInMs = new Date(today) - new Date(mysqlDate)
             const diffInDays = diffInMs / (1000 * 60 * 60 * 24);
 
+            let reward = 50
+
 
             if (2 > diffInDays && diffInDays >= 1) {
+                reward = reward * dailyInfo.streak
+                if (reward > 500) reward = 500;
+                await interaction.client.databaseEditData(`update users set units = units + ${reward} where user_id = '${interaction.user.id}'`)
                 await interaction.client.databaseEditData(`update user_daily set streak = streak + 1, next_date = '${today.toJSON().slice(0, 10)}' where user_id = '${interaction.user.id}'`)
-                return await interaction.reply({ embeds: [interaction.client.greenEmbed("You have successfully claimed your daily reward.", "Successfull")], components: [] })
+                return await interaction.reply({ embeds: [interaction.client.greenEmbed("You have successfully claimed the following reward(s):```json\nUnits: " + reward + "```Daily streak```css\n" + (dailyInfo.streak + 1) + "```", "Daily Reward")], components: [] })
             } else if (diffInDays > 2) {
+                await interaction.client.databaseEditData(`update users set units = units + ${reward} where user_id = '${interaction.user.id}'`)
                 await interaction.client.databaseEditData(`update user_daily set streak = 1, next_date = '${today.toJSON().slice(0, 10)}' where user_id = '${interaction.user.id}'`)
-                return await interaction.reply({ embeds: [interaction.client.greenEmbed("You have successfully claimed your daily reward.", "Successfull")], components: [] })
+                return await interaction.reply({ embeds: [interaction.client.greenEmbed("You have successfully claimed the following reward(s):```css\nUnits" + reward + "```Daily streak```css\n1```", "Daily Reward")], components: [] })
             } else {
                 return await interaction.reply({ embeds: [interaction.client.redEmbed("Daily already claimed. Come back tomorrow.", "Claimed!")], components: [] })
             }
